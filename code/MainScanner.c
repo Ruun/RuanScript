316f21719e82496e788f65223758219bf0796d43
Ruun <emilsimo12@gmail.com>
2024-07-09 22:34:40 -0400

    add scanners files to github

---
+++ code/MainScanner.c
@@ -0,0 +1,272 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Summer, 2024
+* Author: TO_DO
+* Professors: Paulo Sousa
+************************************************************
+#
+# ECHO "=---------------------------------------="
+# ECHO "|  COMPILERS - ALGONQUIN COLLEGE (S24)  |"
+# ECHO "=---------------------------------------="
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@           %&@@@@@@@@@@@     @@    ”
+# ECHO "    @@       @%% (@@@@@@@@@  @     @@    ”
+# ECHO "    @@      @& @   @ @       @     @@    ”
+# ECHO "    @@     @ @ %  / /   @@@@@@     @@    ”
+# ECHO "    @@      & @ @  @@              @@    ”
+# ECHO "    @@       @/ @*@ @ @   @        @@    ”
+# ECHO "    @@           @@@@  @@ @ @      @@    ”
+# ECHO "    @@            /@@    @@@ @     @@    ”
+# ECHO "    @@     @      / /     @@ @     @@    ”
+# ECHO "    @@     @ @@   /@/   @@@ @      @@    ”
+# ECHO "    @@     @@@@@@@@@@@@@@@         @@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@         S O F I A           @@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
+# ECHO "                                         "
+# ECHO "[READER SCRIPT .........................]"
+# ECHO "                                         "
+*/
+
+/*
+************************************************************
+* File name: MainScanner.c
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012]
+* Assignment: A22, A32.
+* Date: May 01 2024
+* Purpose: This file is the main code for Scanner (A22)
+* Function list: (...).
+************************************************************
+*/
+
+/*
+ *.............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: Preprocessor directives
+ * The #define _CRT_SECURE_NO_WARNINGS should be used in MS Visual Studio projects
+ * to suppress the warnings about using "unsafe" functions like fopen()
+ * and standard sting library functions defined in string.h.
+ * The define directive does not have any effect on other compiler projects
+ * (Gcc, VSCode, Codeblocks, etc.).
+ *.............................................................................
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+
+#ifndef BUFFER_H_
+#include "Reader.h"
+#endif
+
+#ifndef SCANNER_H_
+#include "Scanner.h"
+#endif
+
+ /*check for ANSI C compliancy */
+#define ANSI_C 0
+#if defined(__STDC__)
+#undef ANSI_C
+#define ANSI_C 1
+#endif
+
+/*
+ * -------------------------------------------------------------
+ *  Global vars and External vars
+ * -------------------------------------------------------------
+ */
+
+ /* Global objects - variables (used in other codes as external) */
+BufferPointer stringLiteralTable;	/* This buffer implements String Literal Table */
+sofia_intg errorNumber;				/* Run-time error number = 0 by default (ANSI) */
+
+/* External objects */
+extern sofia_intg line; /* Source code line numbers - defined in scanner.c */
+extern Token tokenizer(sofia_void);
+
+/*
+ * -------------------------------------------------------------
+ *  Function declarations
+ * -------------------------------------------------------------
+ */
+sofia_void printScannerError(sofia_string fmt, ...);
+sofia_void displayScanner(BufferPointer ptrBuffer);
+sofia_long getScannerFilesize(sofia_string fname);
+sofia_void printToken(Token t);
+
+/*
+************************************************************
+ *  Scanner Main function
+ * Parameters:
+ *   argc / argv = Parameters from command prompt
+ * Return value:
+ *	Success operation.
+ ***********************************************************
+ */
+
+sofia_intg mainScanner(sofia_intg argc, sofia_string* argv) {
+
+	BufferPointer sourceBuffer;		/* Pointer to input (source) buffer */
+	FILE* fileHandler;				/* Input file handle */
+	Token currentToken;				/* Token produced by the scanner */
+	sofia_intg loadSize = 0;			/* The size of the file loaded in the buffer */
+
+	/* Check for correct arrguments - source file name */
+	if (argc <= 2) {
+		/* __DATE__, __TIME__, __LINE__, __FILE__ are predefined preprocessor macros*/
+		printScannerError("Date: %s  Time: %s", __DATE__, __TIME__);
+		printScannerError("Runtime error at line %d in file %s", __LINE__, __FILE__);
+		printScannerError("%s%s", argv[0], ": Missing source file name.");
+		printScannerError("%s", "Usage: <Option=1> <SourceFile>");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Shows debug mode */
+	printf("%s%d%s", "[Debug mode: ", DEBUG, "]\n");
+
+	/* Create a source code input buffer - multiplicative mode */
+	sourceBuffer = readerCreate(READER_DEFAULT_SIZE, READER_DEFAULT_INCREMENT, MODE_MULTI);
+	if (sourceBuffer == NULL) {
+		printScannerError("%s%s", argv[1], ": Could not create source buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Open source file */
+	if ((fileHandler = fopen(argv[2], "r")) == NULL) {
+		printScannerError("%s%s%s", argv[0], ": Cannot open file: ", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Load source file into input buffer  */
+	printf("Reading file %s ....Please wait\n", argv[2]);
+	loadSize = readerLoad(sourceBuffer, fileHandler);
+	if (loadSize == READER_ERROR)
+		printScannerError("%s%s", argv[0], ": Error in loading buffer.");
+
+	/* Close source file */
+	fclose(fileHandler);
+	/* Find the size of the file */
+	if (loadSize == READER_ERROR) {
+		printf("The input file %s %s\n", argv[2], "is not completely loaded.");
+		printf("Input file size: %ld\n", getScannerFilesize(argv[2]));
+	}
+
+	/* Compact and display the source buffer and add SEOF to input program buffer */
+	if ((loadSize != READER_ERROR) && (loadSize != 0)) {
+		if (readerAddChar(sourceBuffer, READER_TERMINATOR)) {
+			displayScanner(sourceBuffer);
+		}
+	}
+
+	/* Create string Literal Table */
+	stringLiteralTable = readerCreate(READER_DEFAULT_SIZE, READER_DEFAULT_INCREMENT, MODE_ADDIT);
+	if (stringLiteralTable == NULL) {
+		printScannerError("%s%s", argv[0], ": Could not create string literals buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Testbed for the scanner and add SEOF to input program buffer*/
+	/* Initialize scanner input buffer */
+	if (startScanner(sourceBuffer)) {
+		printScannerError("%s%s", argv[0], ": Empty program buffer - scanning canceled");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("\nScanning source file...\n\n");
+	printf("Token\t\tAttribute\n");
+	printf("----------------------------------\n");
+	do {
+		currentToken = tokenizer();
+		printToken(currentToken);
+	} while (currentToken.code != SEOF_T);
+
+	/* Print String Literal Table if not empty */
+	printf("\nPrinting string table...\n");
+	printf("----------------------------------\n");
+	if (readerGetPosWrte(stringLiteralTable)) {
+		readerPrint(stringLiteralTable);
+	}
+	printf("\n----------------------------------\n");
+	readerRestore(sourceBuffer);
+	readerRestore(stringLiteralTable);
+	sourceBuffer = stringLiteralTable = NULL;
+	printScannerData(scData);
+	/* Ass2 evaluation only */
+	if (argv[3] != NULL && *argv[3] == 'l')
+		printf("The number of lines is: %d\n", line);
+
+	return (EXIT_SUCCESS);
+}
+
+/*
+************************************************************
+ *  Error printing function with variable number of arguments
+ *  Params: Variable arguments, using formats from C language.
+ *	 - Internal vars use list of arguments and types from stdarg.h
+ *   - NOTE: The format is using signature from C Language
+***********************************************************
+*/
+
+sofia_void printScannerError(sofia_string fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	(void)vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	/* Move to new line */
+	if (strchr(fmt, '\n') == NULL)
+		fprintf(stderr, "\n");
+}
+
+/*
+************************************************************
+ * The function displays buffer contents
+ * Param:
+ *		- Scanner to be displayed.
+ ***********************************************************
+ */
+
+sofia_void displayScanner(BufferPointer ptrBuffer) {
+	printf("\nPrinting buffer parameters:\n\n");
+	printf("The capacity of the buffer is:  %d\n", readerGetSize(ptrBuffer));
+	printf("The current size of the buffer is:  %d\n", readerGetPosWrte(ptrBuffer));
+	printf("\nPrinting buffer contents:\n\n");
+	readerRecover(ptrBuffer);
+	readerPrint(ptrBuffer);
+}
+
+/*
+ ************************************************************
+ * The function gets size of scanner file
+ * Param:
+ *	- Filename
+ * Return:
+ *	- Size of the file
+ ***********************************************************
+ */
+
+sofia_long getScannerFilesize(sofia_string fname) {
+	FILE* fileInput;
+	sofia_long fileLength;
+	fileInput = fopen(fname, "r");
+	if (fileInput == NULL) {
+		printScannerError("%s%s", "Cannot open file: ", fname);
+		return 0L;
+	}
+	fseek(fileInput, 0L, SEEK_END);
+	fileLength = ftell(fileInput);
+	fclose(fileInput);
+	return fileLength;
+}
---
+++ code/Scanner.h
@@ -0,0 +1,308 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Summer, 2024
+* Author: TO_DO
+* Professors: Paulo Sousa
+************************************************************
+#
+# ECHO "=---------------------------------------="
+# ECHO "|  COMPILERS - ALGONQUIN COLLEGE (S24)  |"
+# ECHO "=---------------------------------------="
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@           %&@@@@@@@@@@@     @@    ”
+# ECHO "    @@       @%% (@@@@@@@@@  @     @@    ”
+# ECHO "    @@      @& @   @ @       @     @@    ”
+# ECHO "    @@     @ @ %  / /   @@@@@@     @@    ”
+# ECHO "    @@      & @ @  @@              @@    ”
+# ECHO "    @@       @/ @*@ @ @   @        @@    ”
+# ECHO "    @@           @@@@  @@ @ @      @@    ”
+# ECHO "    @@            /@@    @@@ @     @@    ”
+# ECHO "    @@     @      / /     @@ @     @@    ”
+# ECHO "    @@     @ @@   /@/   @@@ @      @@    ”
+# ECHO "    @@     @@@@@@@@@@@@@@@         @@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@         S O F I A           @@    ”
+# ECHO "    @@                             @@    ”
+# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
+# ECHO "                                         "
+# ECHO "[READER SCRIPT .........................]"
+# ECHO "                                         "
+*/
+
+/*
+************************************************************
+* File name: Scanner.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 – Compilers, Lab Section: [011, 012]
+* Assignment: A22, A32.
+* Date: May 01 2024
+* Purpose: This file is the main header for Scanner (.h)
+* Function list: (...).
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+
+#ifndef READER_H_
+#include "Reader.h"
+#endif
+
+#ifndef SCANNER_H_
+#define SCANNER_H_
+
+#ifndef NULL
+#include <_null.h> /* NULL pointer constant is defined there */
+#endif
+
+/*#pragma warning(1:4001) */	/*to enforce C89 type comments  - to make //comments an warning */
+
+/*#pragma warning(error:4001)*/	/* to enforce C89 comments - to make // comments an error */
+
+/* Constants */
+#define VID_LEN 20  /* variable identifier length */
+#define ERR_LEN 40  /* error message length */
+#define NUM_LEN 5   /* maximum number of digits for IL */
+
+#define RTE_CODE 1  /* Value for run-time error */
+
+/* TO_DO: Define the number of tokens */
+#define NUM_TOKENS 13
+
+/* TO_DO: Define Token codes - Create your token classes */
+enum TOKENS {
+	ERR_T,		/*  0: Error token */
+	MNID_T,		/*  1: Method name identifier token (start: &) */
+	INL_T,		/*  2: Integer literal token */
+	STR_T,		/*  3: String literal token */
+	LPR_T,		/*  4: Left parenthesis token */
+	RPR_T,		/*  5: Right parenthesis token */
+	LBR_T,		/*  6: Left brace token */
+	RBR_T,		/*  7: Right brace token */
+	KW_T,		/*  8: Keyword token */
+	EOS_T,		/*  9: End of statement (semicolon) */
+	RTE_T,		/* 10: Run-time error token */
+	SEOF_T,		/* 11: Source end-of-file token */
+	CMT_T		/* 12: Comment token */
+};
+
+/* TO_DO: Define the list of keywords */
+static sofia_string tokenStrTable[NUM_TOKENS] = {
+	"ERR_T",
+	"MNID_T",
+	"INL_T",
+	"STR_T",
+	"LPR_T",
+	"RPR_T",
+	"LBR_T",
+	"RBR_T",
+	"KW_T",
+	"EOS_T",
+	"RTE_T",
+	"SEOF_T",
+	"CMT_T"
+};
+
+/* TO_DO: Operators token attributes */
+typedef enum ArithmeticOperators { OP_ADD, OP_SUB, OP_MUL, OP_DIV } AriOperator;
+typedef enum RelationalOperators { OP_EQ, OP_NE, OP_GT, OP_LT } RelOperator;
+typedef enum LogicalOperators { OP_AND, OP_OR, OP_NOT } LogOperator;
+typedef enum SourceEndOfFile { SEOF_0, SEOF_255 } EofOperator;
+
+/* TO_DO: Data structures for declaring the token and its attributes */
+typedef union TokenAttribute {
+	sofia_intg codeType;      /* integer attributes accessor */
+	AriOperator arithmeticOperator;		/* arithmetic operator attribute code */
+	RelOperator relationalOperator;		/* relational operator attribute code */
+	LogOperator logicalOperator;		/* logical operator attribute code */
+	EofOperator seofType;				/* source-end-of-file attribute code */
+	sofia_intg intValue;				/* integer literal attribute (value) */
+	sofia_intg keywordIndex;			/* keyword index in the keyword table */
+	sofia_intg contentString;			/* string literal offset from the beginning of the string literal buffer (stringLiteralTable->content) */
+	sofia_real floatValue;				/* floating-point literal attribute (value) */
+	sofia_char idLexeme[VID_LEN + 1];	/* variable identifier token attribute */
+	sofia_char errLexeme[ERR_LEN + 1];	/* error token attribite */
+} TokenAttribute;
+
+/* TO_DO: Should be used if no symbol table is implemented */
+typedef struct idAttibutes {
+	sofia_byte flags;			/* Flags information */
+	union {
+		sofia_intg intValue;				/* Integer value */
+		sofia_real floatValue;			/* Float value */
+		sofia_string stringContent;		/* String value */
+	} values;
+} IdAttibutes;
+
+/* Token declaration */
+typedef struct Token {
+	sofia_intg code;				/* token code */
+	TokenAttribute attribute;	/* token attribute */
+	IdAttibutes   idAttribute;	/* not used in this scanner implementation - for further use */
+} Token;
+
+/* Scanner */
+typedef struct scannerData {
+	sofia_intg scanHistogram[NUM_TOKENS];	/* Statistics of chars */
+} ScannerData, * pScanData;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* EOF definitions */
+#define CHARSEOF0 '\0'
+#define CHARSEOF255 0xFF
+
+/*  Special case tokens processed separately one by one in the token-driven part of the scanner:
+ *  LPR_T, RPR_T, LBR_T, RBR_T, EOS_T, SEOF_T and special chars used for tokenis include _, & and ' */
+
+/* TO_DO: Define lexeme FIXED classes */
+/* These constants will be used on nextClass */
+#define CHRCOL2 '_'
+#define CHRCOL3 '&'
+#define CHRCOL4 '\''
+#define CHRCOL6 '#'
+
+/* These constants will be used on VID / MID function */
+#define MNID_SUF '&'
+#define COMM_SYM '#'
+
+/* TO_DO: Error states and illegal state */
+#define ESNR	8		/* Error state with no retract */
+#define ESWR	9		/* Error state with retract */
+#define FS		10		/* Illegal state */
+
+ /* TO_DO: State transition table definition */
+#define NUM_STATES		10
+#define CHAR_CLASSES	8
+
+/* TO_DO: Transition table - type of states defined in separate table */
+static sofia_intg transitionTable[NUM_STATES][CHAR_CLASSES] = {
+/*    [A-z],[0-9],    _,    &,   \', SEOF,    #, other
+	   L(0), D(1), U(2), M(3), Q(4), E(5), C(6),  O(7) */
+	{     1, ESNR, ESNR, ESNR,    4, ESWR,	  6, ESNR},	// S0: NOAS
+	{     1,    1,    1,    2,	  3,    3,   3,    3},	// S1: NOAS
+	{    FS,   FS,   FS,   FS,   FS,   FS,	 FS,   FS},	// S2: ASNR (MVID)
+	{    FS,   FS,   FS,   FS,   FS,   FS,	 FS,   FS},	// S3: ASWR (KEY)
+	{     4,    4,    4,    4,    5, ESWR,	  4,    4},	// S4: NOAS
+	{    FS,   FS,   FS,   FS,   FS,   FS,	 FS,   FS},	// S5: ASNR (SL)
+	{     6,    6,    6,    6,    6, ESWR,	  7,    6},	// S6: NOAS
+	{    FS,   FS,   FS,   FS,   FS,   FS,	 FS,   FS},	// S7: ASNR (COM)
+	{    FS,   FS,   FS,   FS,   FS,   FS,	 FS,   FS},	// S8: ASNR (ES)
+	{    FS,   FS,   FS,   FS,   FS,   FS,	 FS,   FS}  // S9: ASWR (ER)
+};
+
+/* Define accepting states types */
+#define NOFS	0		/* not accepting state */
+#define FSNR	1		/* accepting state with no retract */
+#define FSWR	2		/* accepting state with retract */
+
+/* TO_DO: Define list of acceptable states */
+static sofia_intg stateType[NUM_STATES] = {
+	NOFS, /* 00 */
+	NOFS, /* 01 */
+	FSNR, /* 02 (MID) - Methods */
+	FSWR, /* 03 (KEY) */
+	NOFS, /* 04 */
+	FSNR, /* 05 (SL) */
+	NOFS, /* 06 */
+	FSNR, /* 07 (COM) */
+	FSNR, /* 08 (Err1 - no retract) */
+	FSWR  /* 09 (Err2 - retract) */
+};
+
+/*
+-------------------------------------------------
+TO_DO: Adjust your functions'definitions
+-------------------------------------------------
+*/
+
+/* Static (local) function  prototypes */
+sofia_intg			startScanner(BufferPointer psc_buf);
+static sofia_intg	nextClass(sofia_char c);					/* character class function */
+static sofia_intg	nextState(sofia_intg, sofia_char);		/* state machine function */
+sofia_void			printScannerData(ScannerData scData);
+Token				tokenizer(sofia_void);
+
+/*
+-------------------------------------------------
+Automata definitions
+-------------------------------------------------
+*/
+
+/* TO_DO: Pointer to function (of one char * argument) returning Token */
+typedef Token(*PTR_ACCFUN)(sofia_string lexeme);
+
+/* Declare accepting states functions */
+Token funcSL	(sofia_string lexeme);
+Token funcIL	(sofia_string lexeme);
+Token funcID	(sofia_string lexeme);
+Token funcCMT   (sofia_string lexeme);
+Token funcKEY	(sofia_string lexeme);
+Token funcErr	(sofia_string lexeme);
+
+/* 
+ * Accepting function (action) callback table (array) definition 
+ * If you do not want to use the typedef, the equvalent declaration is:
+ */
+
+/* TO_DO: Define final state table */
+static PTR_ACCFUN finalStateTable[NUM_STATES] = {
+	NULL,		/* -    [00] */
+	NULL,		/* -    [01] */
+	funcID,		/* MNID	[02] */
+	funcKEY,	/* KEY  [03] */
+	NULL,		/* -    [04] */
+	funcSL,		/* SL   [05] */
+	NULL,		/* -    [06] */
+	funcCMT,	/* COM  [07] */
+	funcErr,	/* ERR1 [06] */
+	funcErr		/* ERR2 [07] */
+};
+
+/*
+-------------------------------------------------
+Language keywords
+-------------------------------------------------
+*/
+
+/* TO_DO: Define the number of Keywords from the language */
+#define KWT_SIZE 10
+
+/* TO_DO: Define the list of keywords */
+static sofia_string keywordTable[KWT_SIZE] = {
+	"data",		/* KW00 */
+	"code",		/* KW01 */
+	"int",		/* KW02 */
+	"real",		/* KW03 */
+	"string",	/* KW04 */
+	"if",		/* KW05 */
+	"then",		/* KW06 */
+	"else",		/* KW07 */
+	"while",	/* KW08 */
+	"do"		/* KW09 */
+};
+
+/* NEW SECTION: About indentation */
+
+/*
+ * Scanner attributes to be used (ex: including: intendation data
+ */
+
+#define INDENT '\t'  /* Tabulation */
+
+/* TO_DO: Should be used if no symbol table is implemented */
+typedef struct languageAttributes {
+	sofia_char indentationCharType;
+	sofia_intg indentationCurrentPos;
+	/* TO_DO: Include any extra attribute to be used in your scanner (OPTIONAL and FREE) */
+} LanguageAttributes;
+
+/* Number of errors */
+sofia_intg numScannerErrors;
+
+/* Scanner data */
+ScannerData scData;
+
+#endif
